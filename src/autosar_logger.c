439ed2240dfea1ab169ae9315eb3ebf77e582839
Abdullah Khan <abdu.uaq@gmail.com>
2023-06-20 03:23:20 -0400

    Create autosar_logger.c
    added autosar_logger.c

---
+++ src/autosar_logger.c
@@ -0,0 +1,21 @@
+#include "autosar_logger.h"
+
+void autosar_logger_init() {
+    // Perform initialization tasks for the AutoSAR Logger
+    // ...
+}
+
+void autosar_log_info(const char* message, ...) {
+    // Log an informational message
+    // ...
+}
+
+void autosar_log_warning(const char* message, ...) {
+    // Log a warning message
+    // ...
+}
+
+void autosar_log_error(const char* message, ...) {
+    // Log an error message
+    // ...
+}
