91848c29a564030e4593c9a8e0555c30d06f3e2e
Abdullah Khan <abdu.uaq@gmail.com>
2023-06-20 03:47:34 -0400

    Update main.c

--- examples/main.c
+++ examples/main.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
 #include "autosar_logger.h"
 
 int main() {
-    // Initialize the AutoSAR Logger
-    autosar_logger_init();
-
-    // Log an informational message
-    autosar_log_info("This is an informational log message");
-
-    // Log a warning message
-    autosar_log_warning("This is a warning log message");
-
-    // Log an error message
-    autosar_log_error("This is an error log message");
-
+    // Initialize the logger
+    loggerInit();
+    
+    // Enable file logging
+    enableFileLogging("log.txt");
+    
+    // Log messages at different levels
+    logMessage("This is a debug message", DEBUG);
+    logMessage("This is an info message", INFO);
+    logMessage("This is a warning message", WARNING);
+    logMessage("This is an error message", ERROR);
+    
+    // Disable file logging
+    disableFileLogging();
+    
     return 0;
 }
