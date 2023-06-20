da5b69af3965ab27012b15fc2842883ef72288eb
Abdullah Khan <abdu.uaq@gmail.com>
2023-06-20 03:57:01 -0400

    Update autosar_logger.c
    added RTOS compatibility

--- src/autosar_logger.c
+++ src/autosar_logger.c
@@ -1,66 +1,41 @@
+// autosar_logger.c
+
 #include "autosar_logger.h"
 
-// File logging flag
-static int fileLoggingEnabled = 0;
+// Define a mutex for thread-safety
+osMutexId_t logMutex;
 
-// File pointer for log file
-static FILE* logFile = NULL;
-
-// Convert log level enum to string
-const char* levelToString(LogLevel level) {
-    switch (level) {
-        case DEBUG: return "DEBUG";
-        case INFO: return "INFO";
-        case WARNING: return "WARNING";
-        case ERROR: return "ERROR";
-        default: return "";
-    }
-}
-
-// Initialize the logger
 void loggerInit() {
-    // TODO: Implement any initialization code if needed
+    // Initialize the log mutex
+    logMutex = osMutexNew(NULL);
 }
 
-// Enable file logging
-void enableFileLogging(const char* fileName) {
-    fileLoggingEnabled = 1;
-    
-    // Open the log file in append mode
-    logFile = fopen(fileName, "a");
-    
-    if (logFile == NULL) {
-        printf("Failed to open log file for writing\n");
-    }
-}
-
-// Disable file logging
-void disableFileLogging() {
-    fileLoggingEnabled = 0;
-    
-    // Close the log file if it's open
-    if (logFile != NULL) {
-        fclose(logFile);
-        logFile = NULL;
-    }
-}
-
-// Log a message
 void logMessage(const char* message, LogLevel level) {
-    // Get the current time
-    time_t now = time(NULL);
-    struct tm* timeInfo = localtime(&now);
-    
-    // Format the timestamp
-    char timestamp[20];
-    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", timeInfo);
-    
-    // Print the log message to console
-    printf("[%s] [%s] %s\n", timestamp, levelToString(level), message);
-    
-    // Log to file if file logging is enabled
-    if (fileLoggingEnabled && logFile != NULL) {
-        fprintf(logFile, "[%s] [%s] %s\n", timestamp, levelToString(level), message);
-        fflush(logFile);
-    }
+    // Acquire the log mutex before accessing shared resources
+    osMutexAcquire(logMutex, osWaitForever);
+
+    // Log message implementation
+
+    // Release the log mutex after accessing shared resources
+    osMutexRelease(logMutex);
+}
+
+void enableFileLogging(const char* filename) {
+    // Acquire the log mutex before accessing shared resources
+    osMutexAcquire(logMutex, osWaitForever);
+
+    // Enable file logging implementation
+
+    // Release the log mutex after accessing shared resources
+    osMutexRelease(logMutex);
+}
+
+void disableFileLogging() {
+    // Acquire the log mutex before accessing shared resources
+    osMutexAcquire(logMutex, osWaitForever);
+
+    // Disable file logging implementation
+
+    // Release the log mutex after accessing shared resources
+    osMutexRelease(logMutex);
 }
